631a31b99e80085201eeb1554b38efb80063c915
Yinfei84 <Yinfei84@gmail.com>
2013-12-09 12:20:41 +0100

    Fixed deck analysis display and created Card object

--- lib/MtgHand.rb
+++ lib/MtgHand.rb
@@ -4,33 +4,51 @@ module MtgHand
 
   class Deck
 
-    def initialize
-      @path = "/decks"
-    end
-
     def self.read(file)
-      @deck = CSV.read(file, headers: true, col_sep: ";")
-      @starting_hand = []
-      @deck.each do |card|
+      @deckfile = CSV.read(file, headers: true, col_sep: ";")
+      @decklist = []
+      @deckfile.each do |card|
         card["Quantity"].to_i.times { puts card["CMC"] + " | " + card["Mana symbols"].to_s + " | " +  card["name"] + " | " +  card["type"] }
-        card["Quantity"].to_i.times { @starting_hand << card["CMC"] + " | " + card["Mana symbols"].to_s + " | " +  card["name"] + " | " +  card["type"] }
+        card["Quantity"].to_i.times {
+          new_card = Card.new
+          new_card.cmc = card["CMC"]
+          new_card.mana_symbols = card["Mana symbols"].to_s
+          new_card.name = card["name"]
+          new_card.type = card["type"]
+          @decklist << new_card
+        }
       end
-
     end
 
     def self.hand
       puts "====================="
       puts "    STARTING HAND    "
       puts "====================="
-      puts @starting_hand.sample(7)
+      hand = @decklist.sample(7)
+      hand.each do |card|
+        puts card.cmc + " | " + card.mana_symbols + " | " + card.name + " | " + card.type
+      end
+      puts "====================="
+      hand_analysis(hand)
+      puts "====================="
     end
 
-    def self.save(file)
-      deckfile = File.new(file, "r")
-      deckfile.path = @path
-      deckfile.save!
+    def self.hand_analysis (hand)
+      lands = 0
+      hand.each do |card|
+        card.type == "land" ? lands += 1 : nil
+      end
+      if lands <= 1
+        puts lands.to_s + " land in your starting hand."
+      else
+        puts lands.to_s + " lands in your starting hand."
+      end
     end
 
+  end
+
+  class Card
+    attr_accessor :cmc, :mana_symbols, :name, :type
   end
 
 end
